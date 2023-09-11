d40358f410ea4fca85b2e4395b058f9655b1e845
to-mrR0w <marci.sch.2006@gmail.com>
2023-09-11 19:59:07 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+// import { useLocation } from "react-router-dom";
+import About from "./pages/About";
+import Supporter from "./pages/Supporter";
+import Products from "./pages/Products";
+import Product from "./pages/Product";
+import Footer from "./components/Footer";
+// import Header from "./components/Header";
+import Home from "./pages/Home";
+
+import { Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/supporter" element={<Supporter />} />
+          <Route path="/products" element={<Products />}>
+            <Route path=":id" element={<Product />} />
+          </Route>
+          <Route path="/about-us" element={<About />} />
+        </Routes>
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
