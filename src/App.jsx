669f16171897870b36f8d9e1d084602afdff0c22
to-mrR0w <marci.sch.2006@gmail.com>
2023-09-12 18:49:18 +0200

    u

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
 // import { useLocation } from "react-router-dom";
-import About from "./pages/About";
-import Supporter from "./pages/Supporter";
-import Products from "./pages/Products";
-import Product from "./pages/Product";
-import Footer from "./components/Footer";
+// import About from "./pages/About";
+// import Supporter from "./pages/Supporter";
+// import Products from "./pages/Products";
+// import Product from "./pages/Product";
+// import Footer from "./components/Footer";
 // import Header from "./components/Header";
 import Home from "./pages/Home";
 
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
       <div>
         Hi
         <Home />
-        <Routes>
-          <Route path="/JC-Website/" element={<Home />} />
+        {/* <Routes>
+          <Route exact path="/" element={<Home />} />
           <Route path="/supporter" element={<Supporter />} />
           <Route path="/products" element={<Products />}>
             <Route path=":id" element={<Product />} />
           </Route>
           <Route path="/about-us" element={<About />} />
         </Routes>
-        <Footer />
+        <Footer /> */}
       </div>
     </>
   );
