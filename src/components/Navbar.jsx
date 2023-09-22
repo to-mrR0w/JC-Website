53c139ac799a408ec033adb2334cbe70a43a86dd
to-mrR0w <marci.sch.2006@gmail.com>
2023-09-22 14:05:11 +0200

    About-us

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import Sidebar from "./Sidebar";
 
 function Navbar() {
+  const [Dropdown, setDropdown] = useState(false);
+
   return (
     <>
-      {" "}
+      
+      {Dropdown && <Sidebar />}
       <nav className="navbar">
         <ul className="navbar_ul">
+          <li>
+            <button onClick={() => setDropdown(!Dropdown)}>Icon</button>
+          </li>
           <li className="navbar_li">
             <Link to="/JC-Website/">Home</Link>
           </li>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,28 @@
 function About() {
   return (
-    <div className="color bg-red-400 bg-center ">
-      Wir sind von der Schule ...
+    <div className="bg-gray-400  p-10 mt-28 m-auto w-3/4">
+      <ul className="justify-between flex items-center ">
+        <li className="">
+          <img src="" alt="" />
+          Schmiedjell Marcel
+        </li>
+        <li>
+          <img src="" alt="" />
+          Karner Laurens
+        </li>
+        <li>
+          <img src="" alt="" />
+          Schütz Tobias
+        </li>
+        <li>
+          <img src="" alt="" />
+          Weninger David
+        </li>
+        <li>
+          <img src="" alt="" />
+          Radic Daniel
+        </li>
+      </ul>
     </div>
   );
 }
