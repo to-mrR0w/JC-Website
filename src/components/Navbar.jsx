5c87938c5173f0c34e033af10d920886cdc62432
to-mrR0w <marci.sch.2006@gmail.com>
2023-09-22 14:13:12 +0200

    icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,18 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Sidebar from "./Sidebar";
+import { FaBeer } from "react-icons/fa";
 
 function Navbar() {
   const [Dropdown, setDropdown] = useState(false);
 
   return (
     <>
-      
       {Dropdown && <Sidebar />}
       <nav className="navbar">
         <ul className="navbar_ul">
-          <li>
-            <button onClick={() => setDropdown(!Dropdown)}>Icon</button>
+          <li className={`${Dropdown ? "text-green-600" : "text-red-400"}`}>
+            <FaBeer onClick={() => setDropdown(!Dropdown)}>Icon</FaBeer>
           </li>
           <li className="navbar_li">
             <Link to="/JC-Website/">Home</Link>
