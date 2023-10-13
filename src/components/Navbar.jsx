0690b4536d942eef83e8ab69e332858d5fe6d0bb
to-mrR0w <marci.sch.2006@gmail.com>
2023-10-13 09:26:30 +0200

    Products

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,18 +15,33 @@ function Navbar() {
           <li className={`${Dropdown ? "text-green-600" : "text-red-400"}`}>
             <Icon_3lines setDropdown={setDropdown} Dropdown={Dropdown} />
           </li>
-          <li className="block text-white text-center py-5 px-4 no-underline">
-            <Link to="/JC-Website/">Home</Link>
-          </li>
-          <li className="block text-white text-center py-5 px-4 no-underline">
-            <Link to="/JC-Website/products">Products</Link>
-          </li>
-          <li className="block text-white text-center py-5 px-4 no-underline">
-            <Link to="/JC-Website/about-us">About-us</Link>
-          </li>
-          <li className="block text-white text-center py-2 px-4 no-underline ">
+          <li className="group">
             <Link
-              className="block text-center py-2 px-4 no-underline"
+              className="block text-white text-center py-5 px-4 no-underline transition duration-400 border border-solid border-ridge border-t-0 border-l-0 border-r-0 border-b-0 border-transparent hover:text-black hover:border-black hover:bg-gray-500 cursor-pointer"
+              to="/JC-Website/"
+            >
+              Home
+            </Link>
+          </li>
+          <li className="group">
+            <Link
+              className="block text-white text-center py-5 px-4 no-underline transition duration-400 border border-solid border-ridge border-t-0 border-l-0 border-r-0 border-b-0 border-transparent hover:text-black hover:border-black hover:bg-gray-500 cursor-pointer"
+              to="/JC-Website/products"
+            >
+              Products
+            </Link>
+          </li>
+          <li className="group">
+            <Link
+              className="block text-white text-center py-5 px-4 no-underline transition duration-400 border border-solid border-ridge border-t-0 border-l-0 border-r-0 border-b-0 border-transparent hover:text-black hover:border-black hover:bg-gray-500 cursor-pointer"
+              to="/JC-Website/about-us"
+            >
+              About-us
+            </Link>
+          </li>
+          <li className="group ">
+            <Link
+              className="block text-white text-center py-5 px-4 no-underline transition duration-400 border border-solid border-ridge border-t-0 border-l-0 border-r-0 border-b-0 border-transparent hover:text-black hover:border-black hover:bg-gray-500 cursor-pointer"
               to="/JC-Website/cart"
             >
               <ShoppingCart />
---
+++ src/components/products.js
@@ -0,0 +1,1 @@
+export const DATA = [{ id: 1, name: "Powerpank", price: 333, productImg:}];
