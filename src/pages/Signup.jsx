111afde300a7f251e39dfe556d912792019b5639
to-mrR0w <marci.sch.2006@gmail.com>
2023-10-23 19:43:31 +0200

    idk

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,14 +2,9 @@ import { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
 function Signup() {
-  const [text, setText] = useState(null);
   const [mail, setMail] = useState(null);
   const [password, setPassword] = useState(null);
 
-  const handleText = (e) => {
-    e.preventDefault();
-    setText(e.target.value);
-  };
   const handleMail = (e) => {
     e.preventDefault();
     setMail(e.target.value);
@@ -20,29 +15,20 @@ function Signup() {
   };
   async function signc() {
     if (
-      text !== null ||
       mail !== null ||
       password !== null ||
-      text !== "" ||
       mail !== "" ||
       password !== "" ||
       password.trim().lenght() !== 0 ||
-      text.trim().lenght() !== 0 ||
       mail.trim().lenght() !== 0
     ) {
+      console.log(1234);
       await createUserWithEmailAndPassword(auth, mail, password);
     }
   }
   return (
     <div>
       <div className="Signup-container">
-        <input
-          id="text"
-          className="input"
-          type="text"
-          placeholder="Your Name"
-          onChange={handleText}
-        ></input>
         <input
           id="mail"
           className="input"
