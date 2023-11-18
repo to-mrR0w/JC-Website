67777e8c81a952e54dc6a73aef9d886200c1bb43
to-mrR0w <marci.sch.2006@gmail.com>
2023-11-18 20:45:02 +0100

    stripe

---
+++ functions/create-payment-intent.cjs
@@ -0,0 +1,41 @@
+// eslint-disable-next-line no-undef
+const stripe = require("stripe")(process.env.SECRET);
+
+// eslint-disable-next-line no-undef, no-unused-vars
+exports.handler = async function (event, context) {
+  try {
+    if (event.httpMethod !== "POST") {
+      return { statusCode: 405, body: "Method Not Allowed" };
+    }
+
+    const { amount, customerDetails } = JSON.parse(event.body);
+
+    if (!amount || !customerDetails) {
+      return {
+        statusCode: 400,
+        body: "Bad Request: Missing amount or customerDetails",
+      };
+    }
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: "eur",
+      payment_method_types: ["card"],
+      metadata: {
+        customerName: customerDetails.name || "",
+        customerAddress: customerDetails.address || "",
+        customerCity: customerDetails.city || "",
+        customerEmail: customerDetails.email || "",
+        customerPhone: customerDetails.phone || "",
+      },
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
+    };
+  } catch (error) {
+    console.error("Error:", error);
+    return { statusCode: 500, body: "Internal Server Error" };
+  }
+};
