a5aef071f77687551ca6ebf5fb77c7ce66264a3a
Morrow <marci.sch.2006@gmail.com>
2024-02-19 13:23:55 +0100

    HomePageText

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,37 @@
-// import { FaQrcode } from " react-icons/fa";
 import Footer from "../components/Footer";
-// import { gsap } from "gsap";
 
-// import { Flip } from "gsap/Flip";
-// import { ScrollTrigger } from "gsap/ScrollTrigger";
-// import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-// import { TextPlugin } from "gsap/TextPlugin";
-
-// gsap.registerPlugin(Flip, ScrollTrigger, ScrollToPlugin, TextPlugin);
 function Home() {
-  // const spin = () => {
-  //   alert("Yo");
-  // };
-
   return (
     <>
       <div className="wrapText flex flex-col md:flex-row overflow-auto">
-        {/* <span className="text-center justify-center">
-          <FaQrcode size={100} />
-        </span> */}
         <section className="text md:w-1/2 md:pr-4 overflow-auto text-2xl">
-          LOREM IPSU LOREM IPSULOREM IPSULOREM IPSULOREM IPSULOREM IPSULOREM
-          IPSULOREM IPSULOREM IPSULOREM IPSULOREM IPSULOREM IPSULOREM IPSULOREM
-          IPSULOREM IPSULOREM IPSULOREM IPSULOREM IPSULOREM IPSULOREM IPSULOREM
-          IPSULOREM IPSULOREM IPSULOREM IPSULOREM IPSULOREM IPSULOREM IPSU{" "}
+          <b>HEY,</b> wir sind eine Junior Company namens EasyCharge infolge
+          unsere Ausbildungen bekommen wir eine einzignartige CHance. Unsere
+          Kenntnisse zu erweitern und gelerntes Wissen einzusetzten.
+        </section>
+        <div className="image md:w-1/3 mt-4 md:mt-0 overflow-auto">
+          {/* Hier wird die Breite des Bildcontainers auf 1/3 der verfügbaren Breite festgelegt */}
+          <img src="/forrest.jpg" alt="Bild" className="w-full h-auto" />
+        </div>
+        <section className="text md:w-1/2 md:pr-4 overflow-auto text-2xl">
+          <b>UNSER,</b> Projekt ist eine wiederverwendbare Tasche inkl.
+          Powerbank induktion die in hauptsächlich größeren Taschen sowie auch
+          in Rucksäcken leicht integriert werden können.
+          <br />
+          <b>Wieso</b> eigentlich
+        </section>
+      </div>
+
+      <div className="wrapText flex flex-col md:flex-row overflow-auto">
+        <section className="text md:w-1/2 md:pr-4 overflow-auto text-2xl">
+          <b>Wieso</b> eigentlich
         </section>
         <div className="image md:w-1/2 mt-4 md:mt-0 overflow-auto">
+          {/* Hier bleibt die Breite des Bildcontainers auf der Hälfte der verfügbaren Breite */}
           <img src="/forrest.jpg" alt="Bild" className="w-full h-auto" />
         </div>
       </div>
+
       <Footer className="mt-auto" />
     </>
   );
