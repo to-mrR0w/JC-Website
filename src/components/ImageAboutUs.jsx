96ea08752ed71ce58c3a8cb0c0cfb00ee49df76e
Morrow <marci.sch.2006@gmail.com>
2024-02-19 23:53:48 +0100

    Changes

--- src/components/ImageAboutUs.jsx
+++ src/components/ImageAboutUs.jsx
@@ -1,9 +1,12 @@
 function ImageAboutUs({ data }) {
   return (
-    <li className="text-center">
-      <img className="rounded-md w-36 h-44" src={data.image} alt={`image/${data.name}`} />
-      <span className="text-lg ">{data.name}</span>
-      <br />
+    <li className="text-center flex flex-col items-center">
+      <img
+        className="rounded-md w-36 h-44"
+        src={data.image}
+        alt={`image/${data.name}`}
+      />
+      <span className="text-lg">{data.name}</span>
       <span className="text-slate-500 text-base">{data.job}</span>
     </li>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,57 +1,76 @@
 import { useEffect } from "react";
-import { ScrollTrigger, gsap } from "gsap/all"; // Make sure to import ScrollTrigger
-
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger"; // Import ScrollTrigger separately
 import Footer from "../components/Footer";
+
+// Register ScrollTrigger plugin
 gsap.registerPlugin(ScrollTrigger);
+
 function Home() {
   useEffect(() => {
-    gsap.fromTo(".text", { opacity: 0.5, y: -100 }, { duration: 1, opacity: 1, y: 0 });
-    gsap.fromTo(".img", { duration: 1, opacity: 0.5, scale: 1, y: 100 }, { opacity: 1, scale: 1, y: 0 });
-  
-    gsap.fromTo(".textWieso", {
-      opacity: 0,
-      x: 500
-    }, {
-      opacity: 1,
-      x: 0,
-      duration: 2,
-      scrollTrigger: {
-        trigger: ".textWieso",
-        start: "top bottom", // adjust this according to your needs
-        toggleActions: "play none none none" // play animation once when triggered
-      }
-    });
+    // Animation for the initial appearance of elements
+    gsap.fromTo(
+      ".text",
+      { opacity: 0.5, y: -100 },
+      { duration: 1, opacity: 1, y: 0 },
+    );
+    gsap.fromTo(
+      ".img",
+      { duration: 1, opacity: 0.5, scale: 1, y: 100 },
+      { opacity: 1, scale: 1, y: 0 },
+    );
+
+    // Animation triggered when ".textWieso" enters the viewport
+    gsap.fromTo(
+      ".textWieso",
+      {
+        opacity: 0,
+        x: 500,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 2,
+        scrollTrigger: {
+          trigger: ".textWieso",
+          start: "top 80%", // Adjusted the start value to trigger animation closer to the bottom          toggleActions: "play none none none", // play animation once when triggered
+        },
+      },
+    );
   }, []);
-  
-  
-  
 
   return (
     <>
       <div className="wrapText flex flex-col md:flex-row overflow-auto">
         <section className="text md:w-1/2 md:pr-4 overflow-auto text-2xl">
           <b>HEY,</b> wir sind eine Junior Company namens EasyCharge infolge
-          unsere Ausbildungen bekommen wir eine einzignartige CHance. Unsere
-          Kenntnisse zu erweitern und gelerntes Wissen einzusetzten.
+          unsere Ausbildungen bekommen wir eine einzigartige Chance. Unsere
+          Kenntnisse zu erweitern und gelerntes Wissen einzusetzen.
         </section>
         <div className="image md:w-1/3 mt-4 md:mt-0 overflow-auto">
-          {/* Hier wird die Breite des Bildcontainers auf 1/3 der verfügbaren Breite festgelegt */}
+          {/* Image container */}
           <img src="/forrest.jpg" alt="Bild" className="w-full h-auto" />
         </div>
         <section className="text md:w-1/2 md:pr-4 overflow-auto text-2xl">
           <b>UNSER,</b> Projekt ist eine wiederverwendbare Tasche inkl.
-          Powerbank induktion die in hauptsächlich größeren Taschen sowie auch
+          Powerbank Induktion, die hauptsächlich in größeren Taschen sowie auch
           in Rucksäcken leicht integriert werden kann.
           <br />
         </section>
       </div>
 
       <div className="wrapText flex flex-col md:flex-row overflow-auto">
-        <section id="why" className="textWieso md:w-1/2 md:pr-4 overflow-auto text-2xl">
-          <b>Wieso</b> eigentlich
+        <section className="textWieso md:w-1/2 md:pr-4 overflow-auto text-2xl">
+          <b>Wieso EasyCharge?</b> Wir haben EasyCharge gegründet, um eine
+          wiederverwendbare Lösung für den täglichen Bedarf und/oder
+          außerhäuslichen Aktivitäten an komfort zu bieten. In einer so
+          vernetzten Welt, werden Akkus immer schneller leer, wir wollen das
+          verhindern. Wieso? Einmal unterwegs beim Wandern und verlaufen? Nun
+          nicht mehr . Unsere Produkte bieten Komfort, Zuverlässigkeit und
+          Umweltfreundlichkeit in einem.
         </section>
         <div className="image md:w-1/2 mt-4 md:mt-0 overflow-auto">
-          {/* Hier bleibt die Breite des Bildcontainers auf der Hälfte der verfügbaren Breite */}
+          {/* Image container */}
           <img src="/forrest.jpg" alt="Bild" className="w-full h-auto" />
         </div>
       </div>
